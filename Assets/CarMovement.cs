53d158de28451563c709abcc1280fd184e882ce4
Lajos de Graaf <lajosdegraaf@hotmail.com>
2017-09-26 19:25:28 +0200

    my Prototype
    Car Simulator

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotSpeed;
+    public float moveV;
+    public float turnH;
+    public float brakeSpeed;
+    public float boostActive;
+
+    public AudioClip horn;
+    public AudioClip crash;
+
+    void Start ()
+    {
+        GetComponents<AudioSource>()[1].clip = horn;
+        GetComponents<AudioSource>()[0].clip = crash;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponents<AudioSource>()[1].Play();
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        turnH = Input.GetAxis("Horizontal") * rotSpeed;
+        transform.Translate(0, 0, moveV * Time.deltaTime);
+        if (moveV != 0)
+        {
+            transform.Rotate(0, turnH, 0 * Time.deltaTime);
+        }
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponents<AudioSource>()[0].Play();
+    }
+}
---
+++ Assets/crashme.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crashme : MonoBehaviour {
+
+    
+
+    void Start()
+    {
+        //GetComponent<AudioSource>().clip = crash;
+
+    }
+
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
